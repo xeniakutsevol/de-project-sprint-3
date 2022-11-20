55bc93ff787c092d2670d5dd3770b75ca89a473c
Ксения Куцевол <xeniakutsevol@gmail.com>
2022-11-21 02:02:24 +0300

    project to review

---
+++ dags/sql/mart.f_customer_retention.sql
@@ -0,0 +1,81 @@
+-- create table 
+-- create table mart.f_customer_retention(
+-- new_customers_count int,
+-- returning_customers_count int,
+-- refunded_customer_count int,
+-- period_name varchar(10),
+-- period_id varchar(100),
+-- item_id int,
+-- new_customers_revenue numeric(14,2),
+-- returning_customers_revenue numeric(14,2),
+-- customers_refunded int 
+-- );
+
+-- insert increment
+insert into mart.f_customer_retention (
+new_customers_count,
+returning_customers_count,
+refunded_customer_count,
+period_name,
+period_id,
+item_id,
+new_customers_revenue,
+returning_customers_revenue,
+customers_refunded
+)
+with new_increment as (
+select
+customer_id,
+item_id,
+status,
+count(*) as cnt,
+sum(payment_amount) as revenue
+from staging.user_order_log
+where date_time between (current_date::date-7) and (current_date::date-1)
+group by customer_id, item_id, status
+),
+ids as (
+select distinct item_id
+from new_increment
+),
+new_customers as (
+select
+item_id,
+count(customer_id) as new_customers_count,
+sum(revenue) as new_customers_revenue
+from new_increment
+where status='shipped' and cnt=1
+group by item_id
+),
+refunded_customers as (
+select
+item_id,
+count(customer_id) as refunded_customer_count,
+sum(cnt) as customers_refunded
+from new_increment
+where status='refunded'
+group by item_id
+),
+returning_customers as (
+select
+item_id,
+count(customer_id) as returning_customers_count,
+sum(revenue) as returning_customers_revenue
+from new_increment
+where status='shipped' and cnt>1
+group by item_id
+)
+select
+nc.new_customers_count,
+rc.returning_customers_count,
+rf.refunded_customer_count,
+'weekly' as period_name,
+((current_date::date-7)::varchar(20) || ' - ' || (current_date::date-1)::varchar(20)) as period_id,
+ids.item_id,
+nc.new_customers_revenue,
+rc.returning_customers_revenue,
+rf.customers_refunded
+from ids
+left join new_customers nc using(item_id)
+left join refunded_customers rf using(item_id)
+left join returning_customers rc using(item_id);
---
+++ dags/sql/mart.f_sales.sql
@@ -0,0 +1,17 @@
+-- alter tables add status
+-- alter table staging.user_order_log add column status varchar(20);
+-- alter table mart.f_sales add column status varchar(20);
+
+-- insert increment
+insert into mart.f_sales (date_id, item_id, customer_id, city_id, quantity, payment_amount, status)
+select
+dc.date_id,
+item_id,
+customer_id,
+city_id,
+case when status='refunded' then -1*quantity else quantity end as quantity,
+case when status='refunded' then -1*payment_amount else payment_amount end as payment_amount,
+case when status is null then 'shipped' else status end as status
+from staging.user_order_log uol
+left join mart.d_calendar as dc on uol.date_time::Date = dc.date_actual
+where uol.date_time::Date = '{{ds}}';
